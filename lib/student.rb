39be049846a4ad872ce1ac5a76233e03480d7298
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 21:37:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,20 +8,28 @@ class Student
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
-
     new_student
-
-    # create a new Student object given a row from the database
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    found_student = nil
+    self.all.each do |student|
+      if student.name == name
+      #  binding.pry
+       found_student = student
+      end
+    end
   end
 
   def save
